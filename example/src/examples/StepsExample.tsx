73742eba4146d8263776da9687e30d017ba0742f
vinit_groww <vinitraj1012@gmail.com>
2020-05-10 14:46:14 +0530

    added direction props to steps

--- example/src/examples/StepsExample.tsx
+++ example/src/examples/StepsExample.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { View } from 'react-native';
 // eslint-disable-next-line import/no-unresolved
 import { Steps } from 'supergroww';
 
 const StepsExample = () => {
-  return <Steps />;
+  return (
+    <View>
+      <Steps direction="horizontal" />
+      <Steps direction="vertical" />
+    </View>
+  );
 };
 
 export default StepsExample;
--- src/components/Steps/index.tsx
+++ src/components/Steps/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { StepsProps } from './types';
 
-const Steps = () => {
+const Steps = (props: StepsProps) => {
+  const { direction } = props;
   return (
-    <View>
-      <Text>Steps components</Text>
+    <View
+      style={{ flexDirection: direction === 'horizontal' ? 'row' : 'column' }}
+    >
+      <Text>Steps - 1</Text>
+      <Text>Steps - 2</Text>
+      <Text>Steps - 3</Text>
+      <Text>Steps - 4</Text>
     </View>
   );
 };
